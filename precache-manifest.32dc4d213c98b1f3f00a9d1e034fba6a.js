bb6af75f88e3dbf8d245cb7af80e84160179479e
Ashu103 <abhishu103@gmail.com>
2020-08-08 20:46:57 +0100

    Updates

---
+++ precache-manifest.32dc4d213c98b1f3f00a9d1e034fba6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2eee5e505db37668dc395e86c5b57e5",
+    "url": "/Mons/index.html"
+  },
+  {
+    "revision": "986a0a04a9a559028052",
+    "url": "/Mons/static/css/main.77ca52a4.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/Mons/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Mons/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "986a0a04a9a559028052",
+    "url": "/Mons/static/js/main.728c823e.chunk.js"
+  },
+  {
+    "revision": "041372caa03e5da481c8",
+    "url": "/Mons/static/js/runtime-main.fb57ff68.js"
+  }
+]);
